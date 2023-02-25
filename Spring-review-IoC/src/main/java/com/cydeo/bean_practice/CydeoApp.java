aab8eed1d7cd2ec31ef86d7910063d7652fb34d5
SkrNLALM <cakmakskr@gmail.com>
2023-02-25 20:08:34 +0100

    Task 2 is completed

--- Spring-review-IoC/src/main/java/com/cydeo/bean_practice/CydeoApp.java
+++ Spring-review-IoC/src/main/java/com/cydeo/bean_practice/CydeoApp.java
@@ -7,13 +7,21 @@ public class CydeoApp {
     public static void main(String[] args) {
 
         //how to create container ?
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class, NewAppConfig.class);
 
         FullTimeEmployee ft= applicationContext.getBean(FullTimeEmployee.class);
         ft.createAccount();
 
         PartTimeEmployee pt= applicationContext.getBean(PartTimeEmployee.class);
         pt.createAccount();
+
+        String str2= applicationContext.getBean("str2", String.class);
+        System.out.println(str2);
+
+        String str1= applicationContext.getBean("str1", String.class);
+        System.out.println(str1);
+
+
 
     }
 }
--- Spring-review-IoC/src/main/java/com/cydeo/bean_practice/FullTimeEmployee.java
+++ Spring-review-IoC/src/main/java/com/cydeo/bean_practice/FullTimeEmployee.java
@@ -9,4 +9,5 @@ public class FullTimeEmployee {
     {
         System.out.println("Full Time Employee is created");
     }
+
 }
