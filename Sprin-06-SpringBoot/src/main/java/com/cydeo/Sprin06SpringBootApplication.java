f15ac7f8cbf0f573ba863e24da1508faa840de5f
SkrNLALM <cakmakskr@gmail.com>
2023-02-26 20:03:32 +0100

    SpringBoot created

---
+++ Sprin-06-SpringBoot/src/main/java/com/cydeo/Sprin06SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprin06SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprin06SpringBootApplication.class, args);
+	}
+
+}
